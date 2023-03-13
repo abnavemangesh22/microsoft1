739078cc87ea239484c9efe496ab00375a60cf9d
abnavemangesh22 <69311384+abnavemangesh22@users.noreply.github.com>
2023-03-13 19:22:44 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World! from Thetips4you");
+        }
+    }
+}
